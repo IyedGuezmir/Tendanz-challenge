72f3fc6e45f32748374a05dde25925257f5c02cd
Iyeeed <iyed.guezmir@esprit.tn>
2025-12-31 14:56:33 +0100

    Implement reranking in main function for improved result accuracy

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from app.src.retrieval import rag  # now works
+from app.src.retrieval import rag 
+from app.src.retrieval import reranker
 
 def main():
+
     query_text = "Que se passe-t-il en cas de panne du véhicule ?"
     results = rag.query(query_text, k=4)
+    top_results = reranker.rerank(query_text, results, top_k=4)
+
 
     for i, doc in enumerate(results, 1):
         print(f"\n--- Result {i} ---")
+        print(doc.page_content[:500])
+    
+    print("\n=== After Reranking ===")
+    for i, doc in enumerate(top_results, 1):
+        print(f"\n--- Reranked Result {i} ---")
         print(doc.page_content[:500])
 
 if __name__ == "__main__":
